cc5741eb86aa5bc3857b12d61553c09b5fe1d44e
Kenny Woodson <kwoodson@redhat.com>
2019-07-14 10:42:56 -0400

    chapter 1

---
+++ 01/src/monkey/token/token.go
@@ -0,0 +1,27 @@
+package token
+
+const (
+	ILLEGAL = "ILLEGAL"
+	EOF     = "EOF"
+
+	// Identifier + literals
+	IDENT = "IDENT"
+	INT   = "INT"
+
+	// Operators
+	ASSIGN = "="
+	PLUS   = "+"
+
+	// Delimiters
+	COMMA     = ","
+	SEMICOLON = ";"
+
+	LPAREN = "("
+	RPAREN = ")"
+	LBRACE = "{"
+	RBRACE = "}"
+
+	// Keywords
+	FUNCTION = "FUNCTION"
+	LET      = "LET"
+)
