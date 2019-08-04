8329b6456815a54eb73d2113505d85d390a7e78b
Kenny Woodson <kwoodson@redhat.com>
2019-08-04 11:27:10 -0400

    added return statements

--- 02/src/monkey/ast/ast.go
+++ 02/src/monkey/ast/ast.go
@@ -3,48 +3,56 @@ package ast
 import "monkey/token"
 
 type Program struct {
-  Statements []Statement
+	Statements []Statement
 }
 
 type Node interface {
-  TokenLiteral() string
+	TokenLiteral() string
 }
 
 type Statement interface {
-  Node
-  statementNode()
+	Node
+	statementNode()
 }
 
 type Expression interface {
-  Node
-  expressionNode()
+	Node
+	expressionNode()
 }
 
 type Identifier struct {
-    Token token.Token
-    Value string
+	Token token.Token
+	Value string
 }
 
 type LetStatement struct {
-    Token token.Token
-    Name *Identifier
-    Value Expression
+	Token token.Token
+	Name  *Identifier
+	Value Expression
 }
 
 func (p *Program) TokenLiteral() string {
-  if len(p.Statements) > 0 {
-      return p.Statements[0].TokenLiteral()
-  } else {
-    return ""
-  }
+	if len(p.Statements) > 0 {
+		return p.Statements[0].TokenLiteral()
+	} else {
+		return ""
+	}
 }
 
 func (ls *LetStatement) statementNode() {}
 func (ls *LetStatement) TokenLiteral() string {
-    return ls.Token.Literal
+	return ls.Token.Literal
 }
 
 func (ls *Identifier) expressionNode() {}
 func (ls *Identifier) TokenLiteral() string {
-    return ls.Token.Literal
+	return ls.Token.Literal
 }
+
+type ReturnStatement struct {
+	Token       token.Token
+	ReturnValue Expression
+}
+
+func (rs *ReturnStatement) statementNode()       {}
+func (rs *ReturnStatement) TokenLiteral() string { return rs.Token.Literal }
